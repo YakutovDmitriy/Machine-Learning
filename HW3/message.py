081540c87a397179df0cd55732669c1be12a2bc8
Yakutov Dmitry <miti_95@mail.ru>
2017-10-24 20:00:06 +0300

    hw3 started

--- HW3/message.py
+++ HW3/message.py
@@ -1,18 +1,30 @@
 import sys
 import os
 
+class MessageType:
+    spam = "spam"
+    legit = "legit"
+    unknown = "unknown"
+
 class Message:
-    __slots__ = ['subject', 'text']
-    def __init__(self, subject, text):
+    __slots__ = ['subject', 'text', 'spam', 'mtype']
+    def __init__(self, subject, text, mtype):
         self.subject = subject
         self.text = text
+        self.mtype = mtype
     def __str__(self):
-        return ("Subject: %s" % ' '.join(self.subject) + '\n\n' + ' '.join(self.text))
+        return ("Subject (%s): %s" % (self.mtype, ' '.join(map(str, self.subject))) + '\n\n' + ' '.join(map(str, self.text)))
 
-def parse_message(all):
+def parse_message(filename, all):
     subject = list(map(int, all[0].split()[1:]))
     text = list(map(int, ''.join(all[1:]).split()))
-    return Message(subject, text)
+    if 'spmsg' in filename:
+        mtype = MessageType.spam
+    elif 'legit' in filename:
+        mtype = MessageType.legit
+    else:
+        mtype = MessageType.unknown
+    return Message(subject, text, mtype)
 
 def read_messages():
     cwd = os.path.dirname(os.path.abspath(__file__))
@@ -21,5 +33,5 @@ def read_messages():
         ret.append([])
         for fn in os.listdir(cwd + '\\input\\' + part):
             with open(cwd + '\\input\\' + part + '\\' + fn, 'r') as f:
-                ret[-1].append(parse_message(list(f)))
+                ret[-1].append(parse_message(fn, list(f)))
     return ret
